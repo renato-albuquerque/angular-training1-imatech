b1d0202b71d6b4ae7b5b773c190cf6966f389840
renato albuquerque <renatomalb83@gmail.com>
2023-12-31 06:33:29 -0300

    commit all classes, ima tech

---
+++ src/app/componente4/componente4.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-componente4',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './componente4.component.html',
+  styleUrl: './componente4.component.css'
+})
+export class Componente4Component {
+
+    // Variável para armazenar um texto
+    texto:string = '';
+
+    // Variáveis para armazenar os inputs do formulário
+    email:string = '';
+    senha:string = '';
+
+}
